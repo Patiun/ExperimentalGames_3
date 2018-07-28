73ee9a743e02ab36eee6bd9648372ee2727c6265
Greg Kilmer <Remlik.mk@gmail.com>
2018-07-27 21:32:56 -0400

    Child player controls basically set up, unhappy with how the controls feel but it might be this older compter

---
+++ Assets/Scripts/ChildPlayer/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float rotationSpeed;
+
+    private Rigidbody rb;
+    private string xDir = "Horizontal";
+    private string zDir = "Vertical";
+    private string turnAxis = "Turn";
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move(Input.GetAxis(xDir),Input.GetAxis(zDir));
+        Rotate(Input.GetAxis(turnAxis) * rotationSpeed);
+	}
+
+    public void Move(float x, float z)
+    {
+        rb.velocity = (transform.forward * z + transform.right * x).normalized * speed;
+    }
+
+    public void Rotate(float angles)
+    {
+        Quaternion rot = transform.rotation;
+        rot.y += angles;
+        transform.rotation = rot;
+    }
+}

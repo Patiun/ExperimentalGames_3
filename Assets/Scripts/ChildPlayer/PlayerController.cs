48a8d0e9cc0f50e30b98f800ffbe76737c5136f6
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2018-07-30 16:33:43 -0400

    adjustments

--- Assets/Scripts/ChildPlayer/PlayerController.cs
+++ Assets/Scripts/ChildPlayer/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour {
     private string zDir = "Vertical";
     private string turnAxis = "Turn";
 
+    public Animator childAnim;
+    public GameObject child;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
@@ -29,7 +32,23 @@ public class PlayerController : MonoBehaviour {
 
     public void Move(float x, float z)
     {
-        rb.velocity = (transform.forward * z + transform.right * x).normalized * speed;
+        bool still = rb.velocity.magnitude > (0.5f * speed);
+        rb.velocity = (transform.forward * z * -1 + transform.right * x * -1).normalized * speed;
+        child.transform.forward = rb.velocity.normalized * -1;
+
+        bool change = rb.velocity.magnitude > (0.5f * speed);
+
+        if (still && change)
+        {
+            childAnim.SetInteger("state", 1);
+
+
+        }
+        else if(!still && !change)
+        {
+            childAnim.SetInteger("state", 0);
+            
+        }
     }
 
     public void Rotate(float angles)
